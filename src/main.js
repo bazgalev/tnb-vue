1de683a838e23a40fc6258a1bcae35c7ef923f94
pazgalev <pazgalev@tutu.tech>
2023-03-20 21:51:06 +0300

    ya metrika

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueYandexMetrika from 'vue-yandex-metrika'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(VueYandexMetrika, {
+    id: 92865274,
+    router: router,
+    env: process.env.NODE_ENV
+    // other options
+})
 
 app.mount('#app')
 
